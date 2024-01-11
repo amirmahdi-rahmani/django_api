8c3a93fd61323712ab6e3cfc63c5960b74e0b06d
amirmahdi <rahmani.a92m@gmail.com>
2024-01-11 11:35:13 +0330

    add * to known host

--- introduction/settings.py
+++ introduction/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a==l8qk^$^jqb!yzju7hh-w-i##8mn^rn=tn1oj)5oehi74mxg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
